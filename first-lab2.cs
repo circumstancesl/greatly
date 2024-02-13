9cb677963fc096601e377fc3878c15a7dd2e9ec8
circumstancesl <145998442+circumstancesl@users.noreply.github.com>
2024-02-13 17:59:38 +0300

    Create first-lab2.cs

---
+++ first-lab2.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program {
+  static void Main() {
+    Console.WriteLine("Введите число");
+    int Number = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите степень числа");
+    int Pow = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число x, больше или равно 100");
+    string NumberX = Console.ReadLine();
+
+    int Result = Number;
+    string NumberN;
+    
+    for (int NumberIndex = 1; NumberIndex < Pow; ++NumberIndex) {
+      Result *= Number;
+    }
+
+    char SecondDigit = NumberX[1];
+    NumberN = NumberX.Remove(1, 1) + SecondDigit;
+
+    Console.WriteLine($"{Number} в степени {Pow} = {Result}");
+    Console.WriteLine($"Число N: {NumberN}");
+  }
+}
